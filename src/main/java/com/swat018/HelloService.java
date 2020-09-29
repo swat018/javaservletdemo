3bd1c8f8a6f17121af4c32f768ffdc7864e341f9
jinwoo park <swat018@gmail.com>
2020-09-30 03:07:03 +0900

    스프링 웹 MVC - 스프링 IoC 컨테이너 연동

---
+++ src/main/java/com/swat018/HelloService.java
@@ -0,0 +1,14 @@
+package com.swat018;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author jinwoopark
+ */
+@Service
+public class HelloService {
+
+    public String getName() {
+        return "jinwoo";
+    }
+}
